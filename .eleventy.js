f7b68440b04b00ab784f2161572e22c917d69998
Andrew Davis <atd285@gmail.com>
2021-11-15 22:35:01 -0500

    config

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,38 @@
 const lodashGet = require('lodash/get');
 const yaml = require('js-yaml');
+const Image = require('@11ty/eleventy-img');
+const path = require('path');
 
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassThroughCopy('src/site/img');
-  eleventyConfig.addPassThroughCopy({
-    'src/js': 'js',
-    'src/css': 'css',
+const imageShortcode = async (
+  relativeSrc,
+  alt,
+  className,
+  widths = [null, 400, 800, 1280],
+  formats = ['jpeg', 'webp'],
+  sizes = '100vw'
+) => {
+  // we'll fill this in shortly
+  const { dir: imageDir } = path.parse(relativeSrc);
+  const fullSrc = path.join('src', relativeSrc);
+  const imageMetadata = await Image(fullSrc, {
+    widths,
+    formats,
+    outputDir: path.join('_site', imgDir),
+    urlPath: imgDir,
   });
-  const { DateTime } = require('luxon');
+  console.log(metadata);
+};
+
+module.exports = (eleventyConfig) => {
+  eleventyConfig.addShortcode('image', imageShortcode);
+  eleventyConfig.addPassthroughCopy('assets/images');
+  return {
+    markdownTemplateEngine: 'md',
+    htmlTemplateEngine: '.njk',
+    dir: {
+      input: 'index.njk',
+      output: '_site',
+      include: '_includes',
+    },
+  };
 };
